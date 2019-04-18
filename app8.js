7f3ade8a798ece135316143fa30a014ccc86f142
Wang Xin <17634029450@163.com>
2019-04-18 15:49:13 +0800

    对mysql的query方法promise化封装，不依赖三方库

--- app8.js
+++ app8.js
@@ -3,7 +3,6 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const mysql = require('mysql');
 const body = require('koa-body'); // 用来解析post参数
-const co = require('co-mysql'); //将mysql的回调模式Promise化，改造为async/await模式
 const path = require('path');
 const static = require('koa-static');
 
@@ -12,22 +11,44 @@ const router = new Router();
 
 app.use(body());
 
-let connections = mysql.createPool({
-  // connectionLimit : 10, // 用createPool时默认一次建立10条连接的连接池
+let pool = mysql.createPool({
+  // connectionLimit : 10, // 一次创建的最大连接数
   host: 'localhost',
   user: 'root',
   password: '123456',
   database : 'user'
 });
 
-let db = co(connections);
 
-app.context.db = db
+// promise封装查询方法，扩展查询完成自动释放链接
+pool.Query = function (sql, pramas){
+  let self = this;
+  return new Promise((resolve, reject)=>{
+    self.getConnection((err, conn) => {
+      if(err){
+        reject(err)
+      }else{
+        conn.query(sql, pramas, (err, data) => {
+          conn.release();
+          if(!err){
+            resolve(data)
+          }else{
+            reject(err);
+          }
+        })
+      }
+    })
+
+  })
+}
+
+
+app.context.db = pool;
 
 
 async function verify(name, password, ctx){
   try {
-    const data = await ctx.db.query('SELECT ID, password FROM account WHERE userName=?', [name]);
+    const data = await ctx.db.Query('SELECT ID, password FROM account WHERE userName=?', [name]);
     if(!data.length){
       ctx.body = { code: 1, msg: '用户不存在'};
       return false;
@@ -46,7 +67,7 @@ async function verify(name, password, ctx){
 
 router.get('/api/users', async (ctx)=>{
   try {
-    const result = await ctx.db.query('SELECT * FROM account');
+    const result = await ctx.db.Query('SELECT * FROM account');
     ctx.body = result;
   }catch(e){
     ctx.throw(500, 'database error')
@@ -57,12 +78,12 @@ router.get('/api/users', async (ctx)=>{
 router.post('/api/register', async (ctx)=>{
   const { name, password } = ctx.request.body;
   try {
-    const data = await ctx.db.query('SELECT ID, password FROM account WHERE userName=?', [name]); // ?占位符防止sql注入
+    const data = await ctx.db.Query('SELECT ID, password FROM account WHERE userName=?', [name]); // ?占位符防止sql注入
     if(data.length){
       ctx.body = { code: 1, msg: '用户名已被占用'};
       return;
     }
-    await ctx.db.query('INSERT INTO account (userName, password) VALUES(?, ?)', [name, password]);
+    await ctx.db.Query('INSERT INTO account (userName, password) VALUES(?, ?)', [name, password]);
     ctx.body = {code: 0, msg: '注册成功'};
   }catch(e){
     console.log(e);
@@ -84,7 +105,7 @@ router.post('/api/destory', async (ctx)=>{
   let result = await verify(name, password, ctx);
   if(result){
     try {
-      await ctx.db.query('DELETE FROM account WHERE ID=?', [result[0].ID]);
+      await ctx.db.Query('DELETE FROM account WHERE ID=?', [result[0].ID]);
       ctx.body = {code: 0, msg: '注销成功'};
     }catch(e){
       console.log(e);
@@ -101,7 +122,7 @@ router.post('/api/update', async (ctx)=>{
   let result = await verify(name, password, ctx);
   if(result){
     try {
-      await ctx.db.query('UPDATE account SET password=? WHERE ID=?', [newpass, result[0].ID]);
+      await ctx.db.Query('UPDATE account SET password=? WHERE ID=?', [newpass, result[0].ID]);
       ctx.body = {code: 0, msg: '修改成功'};
     } catch (error) {
       ctx.throw(500, 'database error')
