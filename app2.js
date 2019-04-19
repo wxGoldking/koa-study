950e7862e17c08a9d1cf713c1f0210645891f651
Wang Xin <17634029450@163.com>
2019-04-19 19:26:22 +0800

    v1.0.0

--- app2.js
+++ app2.js
@@ -5,15 +5,15 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-router.get('/', async ctx => {
+router.post('/', async ctx => {
   ctx.body = 'Hello Router';
 })
 
 router.post('/todo', async ctx => {
   ctx.body = 'Todo';
 })
-app.use(router.routes()).use(router.allowedMethods()); // use(router.allowedMethods()) 会在接口地址正确，请求方法错误时返回Method Not Allowed
+app.use(router.routes()).use(router.allowedMethods()); // use(router.allowedMethods()) 会在接口地址正确，请求方法错误时返回Method Not Allowed 405
 
-app.listen(3000, () => { 
-  console.log('listening on 3000 success!') 
+app.listen(3000, () => {
+  console.log('listening on 3000 success!')
 })
